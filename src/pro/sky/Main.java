d55b0c80e675e56c2324a4b5b9b525fc12e37769
sspichuzhkin <103836818+sspichuzhkin@users.noreply.github.com>
2022-05-10 17:09:06 +0300

    hm init (#1)
    * hm init
    
    * hm refactor
    
    * hm refactor 2
    
    * hm error fix 1

--- src/pro/sky/Main.java
+++ src/pro/sky/Main.java
@@ -1,7 +1,48 @@
 package pro.sky;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Task1();
+        Task2();
+        Task3();
+    }
+
+    public static void Task1() {
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+    }
+
+    public static void Task2() {
+
+        String fullName = "Ivanov Ivan Ivanovich";
+        String fullNameForReport = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameForReport);
+    }
+
+    public static String changeCharInPosition(int position, char ch, String str) {
+        char[] charArray = str.toCharArray();
+        charArray[position] = ch;
+        return new String(charArray);
+    }
+    public static void Task3() {
+
+        String fullName = "Иванов Семён Семёнович";
+        String[] words = fullName.split(" ");
+        StringBuilder partOfPhrase = new StringBuilder();
+
+        for (String word : words) {
+            while (word.contains("ё")) {
+                int indexValue = word.indexOf("ё");
+                word = changeCharInPosition(indexValue, 'е', word);
+            }
+            partOfPhrase.append(word).append(" ");
+        }
+        System.out.println("Данные ФИО сотрудника — " + partOfPhrase);
     }
 }
